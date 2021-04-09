dd855cfea10b2e745fcd73eca24bc26a026a41fa
Raphael Queiroz <queiroz.rphl@gmail.com>
2021-04-09 15:06:59 -0300

    adicionando parametros para rodar com o dataset

--- train.py
+++ train.py
@@ -368,9 +368,9 @@ def train(hyp, opt, device, tb_writer=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov4.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
-    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path')
+    parser.add_argument('--weights', type=str, default='', help='initial weights path')
+    parser.add_argument('--cfg', type=str, default='cfg/yolov4-pacsp.cfg', help='model.yaml path')
+    parser.add_argument('--data', type=str, default='data/coco_scaled.yaml', help='data.yaml path')
     parser.add_argument('--hyp', type=str, default='', help='hyperparameters path, i.e. data/hyp.scratch.yaml')
     parser.add_argument('--epochs', type=int, default=300)
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
